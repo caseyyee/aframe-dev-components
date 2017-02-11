194ee40945a00da64916329088ff6ef8ba2eec1b
caseyyee <caseyyee.ca@gmail.com>
2017-02-10 16:30:37 -0800

    wip simple vr-editor

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ window.AFRAME = require('aframe');
 AFRAME.registerComponent('retain-camera', require('./src/retain-camera'));
 
 AFRAME.registerComponent('axis', require('./src/axis'));
+
+AFRAME.registerComponent('controllable', require('./src/controllable'));
+
+AFRAME.registerComponent('selected', require('./src/selected'));
+
+AFRAME.registerComponent('cloneable', require('./src/cloneable'));
